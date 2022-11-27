d263d1ac6871f475a27cb9ead58c8d52dfd7f307
naegonggae <https://github.com/naegonggae/>
2022-11-28 00:25:03 +0900

    first commit

---
+++ src/main/java/com/mustache/hospitals/MustacheHospitalsApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.hospitals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MustacheHospitalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MustacheHospitalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustache/hospitals/MustacheHospitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.hospitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MustacheHospitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
